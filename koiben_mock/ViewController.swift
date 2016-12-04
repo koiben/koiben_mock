b157b855f7b7a37cce40ab1358a4f672a6a77e43
ToYouTaro <yutaro@nagatayuutarou-no-MacBook-Air.local>
2016-12-04 18:21:00 +0900

    1820

---
+++ koiben_mock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  koiben_mock
+//
+//  Created by 永田祐太郎 on 2016/12/04.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
